5fd76db7d192b05a844cfb7ba9b3c6a1166bbd66
Ruslan Salmanov <flainof@gmail.com>
2025-11-11 03:15:07 +0300

    refactor: remove VerificationForm and related files to streamline widget structure

--- src/widgets/verification-form/index.ts
+++
@@ -1,2 +0,0 @@
-export { View as VerificationForm } from './model/view';
-export { VerificationFormSkeleton } from './ui/skeleton';
--- src/widgets/verification-form/model/view.ts
+++
@@ -1,3 +0,0 @@
-import { lazy } from 'react';
-
-export const View = lazy(() => import('../ui/ui').then((module) => ({ default: module.VerificationForm })));
