fe6d61ff6cd60259180956d8d50686202be63b43
Ruslan Salmanov <flainof@gmail.com>
2025-11-13 00:14:40 +0300

    refactor: enhance Loader component with min-h-dvh class and simplify MainLayout structure

--- src/shared/ui/loader.tsx
+++ src/shared/ui/loader.tsx
@@ -2,7 +2,7 @@ import { Container } from "./container";
 import { Typography } from "./typography";
 
 export const Loader = () => (
-    <Container className='flex items-center justify-center'>
+    <Container className='flex items-center justify-center min-h-dvh'>
         <Typography variant="primary" size="4xl" weight="semibold" className="animate-pulse">Загрузка</Typography>
     </Container>
 );
--- src/shared/ui/main-layout.tsx
+++ src/shared/ui/main-layout.tsx
@@ -1,11 +1,8 @@
-import { Container } from './container';
 import { Navbar } from './navbar';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => (
     <section className='flex flex-col gap-5 min-h-dvh'>
-        <Container as='div' className='grow-1'>
-            {children}
-        </Container>
+        {children}
         <Navbar />
     </section>
 );
