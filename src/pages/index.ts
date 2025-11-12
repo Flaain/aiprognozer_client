368d306dc09cc7a85d7fa301ddbbd72d70a7e7be
Ruslan Salmanov <flainof@gmail.com>
2025-11-13 00:13:27 +0300

    feat: add StoreScreen to screens export in index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import type { Screens } from '@/shared/providers/screen/types';
 
 import { HomeScreen } from './home';
+import { StoreScreen } from './store';
 
-export const screens: Screens = [HomeScreen];
+export const screens: Screens = [HomeScreen, StoreScreen];
