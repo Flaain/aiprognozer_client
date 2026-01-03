dd41476a9b676236283185d1b6e06b25a177d7be
Ruslan Salmanov <flainof@gmail.com>
2026-01-03 08:06:01 +0300

    feat: added referrals and invite methods

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -2,5 +2,7 @@ import { api } from "@/shared/api";
 import type { ApiDefaultSuccessResponse } from "@/shared/model/types";
 
 export const userApi = {
-    verify: (id: string) => api.post<ApiDefaultSuccessResponse>('/user/verify', undefined, { params: { id } })
+    verify: (id: string) => api.post<ApiDefaultSuccessResponse>('/user/verify', undefined, { params: { id } }),
+    referalls: <T>(cursor?: string) => api.get<T>('/user/referalls', { params: { cursor } }),
+    invite: () => api.get<string>('/user/invite'), 
 }
