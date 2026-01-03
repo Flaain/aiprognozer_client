824bf1a6780d7626fa49dfcc46a6b789c25cbe4a
Ruslan Salmanov <flainof@gmail.com>
2026-01-03 08:04:46 +0300

    feat: added ref code, timeout, timeoutErrorMessage for login method

--- src/entities/auth/api/index.ts
+++ src/entities/auth/api/index.ts
@@ -1,6 +1,10 @@
-import { api } from "@/shared/api";
-import type { User } from "@/shared/model/types";
+import { api } from '@/shared/api';
+import type { User } from '@/shared/model/types';
 
 export const authApi = {
-    login: () => api.post<User>('/auth/login', undefined, { timeout: 10000, timeoutErrorMessage: 'Login timeout' }),
-}
+    login: (ref?: string) => api.post<User>('/auth/login', undefined, {
+        timeout: 10000,
+        timeoutErrorMessage: 'Login timeout',
+        params: { ref }
+    })
+};
